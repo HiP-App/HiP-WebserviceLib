8a438acadb187a4801fb2a7e9d1346fb2bbefacc
Janis Krasemann <janis.krasemann@enpit.de>
2017-06-05 15:29:54 +0200

    Updated the claim extraction to match the changes introduced by auth-v2

--- Auth.cs
+++ Auth.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using System.Security.Principal;
 
@@ -6,16 +8,23 @@ namespace PaderbornUniversity.SILab.Hip.Webservice
 {
 	public static class Auth
 	{
-		// Adds function to get User Id from Context.User.Identity
-		public static string GetUserIdentity(this IIdentity identity)
-		{
-			var claimsIdentity = identity as ClaimsIdentity;
-			if (claimsIdentity == null)
-			{
-				throw new InvalidOperationException("identity not found");
-			}
+        // Adds function to get User Id from Context.User.Identity
+        public static string GetUserIdentity(this IIdentity identity)
+        {
+            var claimsIdentity = identity as ClaimsIdentity;
+            if (claimsIdentity == null) throw new InvalidOperationException("identity not found");
 
-			return claimsIdentity.FindFirst(ClaimTypes.Name).Value;
-		}
-	}
+            var email = claimsIdentity.FindFirst("email");
+            return email.Value;
+        }
+
+        public static List<Claim> GetUserRoles(this IIdentity identity)
+        {
+            var claimsIdentity = identity as ClaimsIdentity;
+            if (claimsIdentity == null) throw new InvalidOperationException("identity not found");
+
+            var role = claimsIdentity.FindAll("role");
+            return role.ToList();
+        }
+    }
 }
