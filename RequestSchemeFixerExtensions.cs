4d400c502d1186ee6c6140f20d722f1a85b9e399
Sven Vinkemeier <svenvinkemeier@live.de>
2017-12-10 14:13:36 +0100

    Added and documented NSwag workarounds (from DataStore), added and documented HTTP request scheme fixer extension method, upgraded to .NET Core 2.0, removed Swashbuckle reference, removed BearerEvents

---
+++ RequestSchemeFixerExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace PaderbornUniversity.SILab.Hip.Webservice
+{
+    public static class RequestSchemeFixerExtensions
+    {
+        /// <summary>
+        /// Fixes the current request's scheme if an "X-Forwarded-Proto"-header is set.
+        /// </summary>
+        /// <remarks>
+        /// Root issue: Clients send requests to our services via HTTPS, but nginx forwards these requests
+        /// to the Docker services via HTTP. In order for our services to know that the request was originally
+        /// using HTTPS, nginx adds an HTTP header "X-Forwarded-Proto: https" to the requests. This method
+        /// registers a middleware which detects this header and sets the <see cref="HttpRequest.Scheme"/>
+        /// property of the current request to "https" accordingly.
+        /// </remarks>
+        public static IApplicationBuilder UseRequestSchemeFixer(this IApplicationBuilder app)
+        {
+            return app.Use(async (context, next) =>
+            {
+                if (context.Request.Headers.TryGetValue("X-Forwarded-Proto", out var xproto))
+                    context.Request.Scheme = xproto;
+                await next();
+
+            });
+        }
+    }
+}
