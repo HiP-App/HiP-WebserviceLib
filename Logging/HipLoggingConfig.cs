0447a08dd6eed4c7960180bbaa1452b7cedb0ad4
Zain ulabidin <zain.maliik@gmail.com>
2018-01-26 01:32:04 +0100

    Created Logging Wrapper

---
+++ Logging/HipLoggingConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaderbornUniversity.SILab.Hip.Webservice.Logging
+{
+    public class HipLogConfig
+    {
+        public string Host { set; get; }
+        public int Port { set; get; }
+        public string LogSource { set; get; }
+    }
+}
